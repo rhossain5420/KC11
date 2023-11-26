0dff691f9da317ee8e55a5591b2d86ea6ee2c3aa
rhossain5420 <rhossain5420@students.mc3.edu>
2023-11-25 21:34:18 -0500

    Update in Java CIS111b class
    This is a new update from me.

--- CIS111b.java
+++ CIS111b.java
@@ -1,5 +1,6 @@
 public class CIS111b {
-public static void main(String[] args){
-System.out.println("Hello world!"); 
-}
+   public static void main(String[] args){
+      System.out.println("Hello world!"); 
+      System.out.println("============"); 
+   }
 }
